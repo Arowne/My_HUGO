f628ee48ecc9665c25bc50e309a1a134e2300d9f
Bear <aaron.y.boateng@gmail.com>
2019-09-18 23:52:59 +0200

    My_hugo=> Unit test coloration updateé

--- unittest.sh
+++ unittest.sh
@@ -9,37 +9,37 @@ photo="photo: \"/img/association.jpg\""
 important="important: false"
 
 if [ "$(grep "$title"  "$FILENAME" )" != "" ]; then
-    echo "found: $title"
+    echo "\033[32m  OK \033[0m $title"
 else
     echo "\033[31m TEST FAILED: \"$title\" \033[0m"
 fi
 
 if [ "$(grep "$domain"  "$FILENAME" )" != "" ]; then
-    echo "found: $domain"
+    echo "\033[32m  OK \033[0m $domain"
 else
    echo "\033[31m TEST FAILED: \"$domain\" \033[0m"
 fi
 
 if [ "$(grep "$address"  "$FILENAME" )" != "" ]; then
-    echo "found: $address"
+    echo "\033[32m  OK \033[0m $address"
 else
     echo "\033[31m TEST FAILED: \"$address\" \033[0m"
 fi
 
 if [ "$(grep "$description"  "$FILENAME" )" != "" ]; then
-    echo "found: $description"
+    echo "\033[32m  OK \033[0m $description"
 else
     echo "\033[31m TEST FAILED: \"$description\" \033[0m"
 fi
 
 if [ "$(grep "$photo"  "$FILENAME" )" != "" ]; then
-    echo "found: $photo"
+    echo "\033[32m  OK \033[0m $photo"
 else
     echo "\033[31m TEST FAILED: \"$photo\" \033[0m"
 fi
 
 if [ "$(grep "$important"  "$FILENAME" )" != "" ]; then
-    echo "found: $important"
+    echo "\033[32m  OK \033[0m $important"
 else
     echo "\033[31m TEST FAILED: \"$important\" \033[0m"
 fi
@@ -62,61 +62,61 @@ draft="draft: false"
 important="important: false"
 
 if [ "$(grep "$title"  "$FILENAME" )" != "" ]; then
-    echo "found: $title"
+    echo "\033[32m  OK \033[0m $title"
 else
     echo "\033[31m TEST FAILED: \"$title\" \033[0m"
 fi
 
 if [ "$(grep "$address"  "$FILENAME" )" != "" ]; then
-    echo "found: $address"
+    echo "\033[32m  OK \033[0m $address"
 else
    echo "\033[31m TEST FAILED: \"$address\" \033[0m"
 fi
 
 if [ "$(grep "$postalCode"  "$FILENAME" )" != "" ]; then
-    echo "found: $postalCode"
+    echo "\033[32m  OK \033[0m $postalCode"
 else
     echo "\033[31m TEST FAILED: \"$postalCode\" \033[0m"
 fi
 
 if [ "$(grep "$city"  "$FILENAME" )" != "" ]; then
-    echo "found: $city"
+    echo "\033[32m  OK \033[0m $city"
 else
     echo "\033[31m TEST FAILED: \"$city\" \033[0m"
 fi
 
 if [ "$(grep "$label"  "$FILENAME" )" != "" ]; then
-    echo "found: $label"
+    echo "\033[32m  OK \033[0m $label"
 else
     echo "\033[31m TEST FAILED: \"$label\" \033[0m"
 fi
 
 if [ "$(grep "$description"  "$FILENAME" )" != "" ]; then
-    echo "found: $description"
+    echo "\033[32m  OK \033[0m $description"
 else
     echo "\033[31m TEST FAILED: \"$description\" \033[0m"
 fi
 
 if [ "$(grep "$photos"  "$FILENAME" )" != "" ]; then
-    echo "found: $photos"
+    echo "\033[32m  OK \033[0m $photos"
 else
     echo "\033[31m TEST FAILED: \"$photos\" \033[0m"
 fi
 
 if [ "$(grep "$important"  "$FILENAME" )" != "" ]; then
-    echo "found: $important"
+    echo "\033[32m  OK \033[0m $important"
 else
     echo "\033[31m TEST FAILED: \"$important\" \033[0m"
 fi
 
 if [ "$(grep "$draft"  "$FILENAME" )" != "" ]; then
-    echo "found: $draft"
+    echo "\033[32m  OK \033[0m $draft"
 else
     echo "\033[31m TEST FAILED: \"$draft\" \033[0m"
 fi
 
 if [ "$(grep "$association"  "$FILENAME" )" != "" ]; then
-    echo "found: $association"
+    echo "\033[32m  OK \033[0m $association"
 else
     echo "\033[31m TEST FAILED: \"$association\" \033[0m"
 fi
@@ -133,25 +133,25 @@ role="role : \"Deputy\""
 photos="photos: \"/img/profil.png\""
 
 if [ "$(grep "$firstname"  "$FILENAME" )" != "" ]; then
-    echo "found: $firstname"
+    echo "\033[32m  OK \033[0m $firstname"
 else
     echo "\033[31m TEST FAILED: \"$firstname\" \033[0m"
 fi
 
 if [ "$(grep "$lastname"  "$FILENAME" )" != "" ]; then
-    echo "found: $lastname"
+    echo "\033[32m  OK \033[0m $lastname"
 else
    echo "\033[31m TEST FAILED: \"$lastname\" \033[0m"
 fi
 
 if [ "$(grep "$role"  "$FILENAME" )" != "" ]; then
-    echo "found: $role"
+    echo "\033[32m  OK \033[0m $role"
 else
     echo "\033[31m TEST FAILED: \"$role\" \033[0m"
 fi
 
 if [ "$(grep "$photos"  "$FILENAME" )" != "" ]; then
-    echo "found: $photos"
+    echo "\033[32m  OK \033[0m $photos"
 else
     echo "\033[31m TEST FAILED: \"$photos\" \033[0m"
 fi
